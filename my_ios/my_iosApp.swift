daff688d97fbc9cd9fe2582e01436a3c887264a9
kimhongseok <drghdtjr@naver.com>
2024-02-11 16:50:18 +0900

    Initial Commit

---
+++ my_ios/my_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_iosApp.swift
+//  my_ios
+//
+//  Created by 김홍석 on 2/11/24.
+//
+
+import SwiftUI
+
+@main
+struct my_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
