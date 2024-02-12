b68ef8c468c7c57afbe11cd61beaf51eb847d483
kimhongseok <drghdtjr@naver.com>
2024-02-12 21:53:24 +0900

    24.02.12

---
+++ my_ios/AppView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  my_ios
+//
+//  Created by 김홍석 on 2/11/24.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        Text("hello")
+    }
+}
+
+#Preview {
+    AppView()
+}
